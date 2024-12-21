949a2a7f920de8e8832d8e31e21e1aff5cfd7c1d
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2024-12-21 00:10:36 -0300

    Anteração de Float para nemeric

--- VoxFlow-Backend/app/config/imports/create_table.py
+++ VoxFlow-Backend/app/config/imports/create_table.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float, Time, Date, Text, Table
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Numeric, Time, Date, Text, Table
 from sqlalchemy.orm import relationship
 
 from sqlalchemy.ext.declarative import declarative_base
